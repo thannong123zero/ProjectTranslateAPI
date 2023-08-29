64b87d73b8be5c78ebe3f6caf50fa604c3734b2f
Mr Zero <nguyenhoangtai2k@gmail.com>
2023-08-29 10:03:38 +0700

    update code

--- ProjectTranslateAPI/Program.cs
+++ ProjectTranslateAPI/Program.cs
@@ -4,7 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
